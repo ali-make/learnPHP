19bdf23b0c28d111eb2b5b81a9bd42e68539c41e
Ali Nur <alinurcowal@gmail.com>
2021-02-23 13:11:06 +0700

    cukup!

---
+++ phps basic/Operasi.php
@@ -0,0 +1,31 @@
+<?php 
+/**
+ * Operator dipakai untuk menjalankan operasi matematika pada variabel
+ * Operator dalam PHP dibagi seperti berikut:
+ * - Aritmetika operator
+ * - Assignment operator
+ * - Comparison operator
+ * - Increment/Decrement operator
+ * - Logical operator
+ * - String operator
+ * - Array operator
+ * - Conditional assignment operator
+ */
+
+ /**
+  * Aritmatika Operator
+  */
+  $a = 4;
+  $b = 5;
+  echo "<h2>";
+
+  echo $a + $b . "<br>";
+  echo $a - $b . "<br>";
+  echo $a * $b . "<br>";
+  echo $a / $b . "<br>";
+  echo $a % $b . "<br>";
+  echo $a ** $b . "<br>";
+
+  echo "</h2>";
+
+  echo "<h2>Berhenti sampai sini!</h2>";
